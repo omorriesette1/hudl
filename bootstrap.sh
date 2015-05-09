404fbba359fea7892f9f6d1b32a02a9e2c397f8e
Orlando Morriesette <orlando@Angelicas-iMac.local>
2015-05-09 14:08:51 -0500

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,46 +24,40 @@ mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'hudl'@'localhost'"
 #sudo su - hudl -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
 
 # Edit hosts file
-echo "192.168.50.10	hudl-prod" >> /etc/hosts
-echo "192.168.50.20	hudl-dev" >> /etc/hosts
+HOST="$(hostname -s)"
+HOSTFILE="/etc/hosts"
+MASTER="prod-master"
+SLAVE="prod-slave"
+DEV="hudl-dev"
 
-# Customize MOTD
-echo "Creating Dynamic MOTD ..."
-cat << 'EOF' > /usr/local/bin/hudl-motd
-PROCCOUNT=`ps -Afl | wc -l`
-PROCCOUNT=`expr $PROCCOUNT - 5`
-GROUPZ=`groups`
-USER=`whoami`
+case ${HOST} in
+    ${MASTER})	
+		echo "192.168.50.10 prod-master" >> ${HOSTFILE}
+		echo "192.168.50.20 prod-slave" >> ${HOSTFILE}
+		;;
+    ${SLAVE})
+		echo "192.168.50.10 prod-master" >> ${HOSTFILE}
+                echo "192.168.50.20 prod-slave" >> ${HOSTFILE}
+		echo "172.168.50.20 hudl-dev" >> ${HOSTFILE}
+		;;
+    ${DEV})
+		echo "172.168.50.10 prod-slave" >> ${HOSTFILE}
+		echo "172.168.50.20 hudl-dev" >> ${HOSTFILE}
+		;;
+    esac
 
-ENDSESSION="Unlimited"
-PRIVLAGED="Regular User"
-Color_off='\e[0m'
-#fi
+# Create Banner
+cat << 'EOF' > /etc/motd
 
-echo -e "\033[1;32m
+echo -e "
 
-* * * * * * * * * * * * W A R N I N G * * * * * * * * * * * *
-           THIS SYSTEM IS FOR DEMO PURPOSES ONLY!
-* * * * * * * * * * * * W A R N I N G * * * * * * * * * * * *
+* * * * * * * * * * * * * * * * * * * * * * * * * *
+          WELCOME TO MY HUDL DEMO!
+          BY: ORLANDO MORRIESETTE
+* * * * * * * * * * * * * * * * * * * * * * * * * *
 
-
-\033[0;35m+++++++++++++++++: \033[0;37mSYSTEM DATA\033[0;35m :++++++++++++++++++++++
-+  \033[0;37mHostname \033[0;35m= \033[1;32m`hostname`
-\033[0;35m+   \033[0;37mAddress \033[0;35m= \033[1;32m`ifconfig eth1 | grep addr:[0-9] | awk '{print $2}' | cut -d: -f2`
-\033[0;35m+    \033[0;37mKernel \033[0;35m= \033[1;32m`uname -r`
-\033[0;35m+    \033[0;37mUptime \033[0;35m=\033[1;32m`uptime | sed 's/.*up ([^,]*),.*/1/'`
-\033[0;35m+       \033[0;37mCPU \033[0;35m= \033[1;32m4x Intel(R) Xeon(R) E5620 @ 2.40GHz
-\033[0;35m+    \033[0;37mMemory \033[0;35m= \033[1;32m`cat /proc/meminfo | grep MemTotal | awk '{print $2}'` kB
-\033[0;35m++++++++++++++++++: \033[0;37mUSER DATA\033[0;35m :++++++++++++++++++++++++
-+  \033[0;37mUsername \033[0;35m= \033[1;32m$USER
-\033[0;35m+ \033[0;37mPrivlages \033[0;35m= \033[1;32m$PRIVLAGED
-\033[0;35m+  \033[0;37mSessions \033[0;35m= \033[1;32m`who | grep $USER | wc -l` of $ENDSESSION MAX
-\033[0;35m+ \033[0;37mProcesses \033[0;35m= \033[1;32m$PROCCOUNT of `ulimit -u` MAX
-\033[0;35m++++++++++++++++++: \033[0;37mLAST LOGIN\033[0;35m :+++++++++++++++++++++++
-+\033[0;37m \033[1;32m`lastlog -u $USER | grep $USER`
-\033[0;35m+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 "
-echo -e $Color_off
 EOF
-chmod +x /usr/local/bin/hudl-motd
-echo "/usr/local/bin/hudl-motd" >> /etc/profile
+
+perl -npe 's/#PrintMotd/PrintMotd/' -i /etc/ssh/sshd_config
+service sshd restart
--- db_configure.py
+++ db_configure.py
@@ -8,8 +8,8 @@ query = db.cursor()
 db_create = "CREATE DATABASE User_Profiles"
 db_useUP = "USE User_Profiles"
 table_create = "CREATE TABLE profiles (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25), password VARCHAR(40), email VARCHAR(30), phone VARCHAR(20))"
-
 table_insert = "INSERT INTO profiles (id, username, password, email, phone) VALUES (NULL,'Walter Payton', SHA1('bears'), 'wpayton@chicagobears.com', '555-134-3434'), (NULL, 'Barry Sanders', SHA1('lions'), 'bsanders@detroitlions.com', '555-120-2020'), (NULL, 'Earl Campbell', SHA1('oilers'), 'ecampbell@houstonoilers.com', '555-120-3434')"
+db_setup = "db_create, db_useUP, table_create, table_insert"
 
-for task in db_create, db_useUP, table_create, table_insert:
-    query.execute(task)
+for tasks in db_setup:
+    query.execute(tasks)
